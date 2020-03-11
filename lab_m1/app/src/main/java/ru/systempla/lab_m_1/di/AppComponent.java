ac958830912fde6739f6df8942a5135430b6f7c6
SystemPLA <amalovatyj@yandex.ru>
2020-03-12 01:07:39 +0300

    added AppModule

--- lab_m1/app/src/main/java/ru/systempla/lab_m_1/di/AppComponent.java
+++ lab_m1/app/src/main/java/ru/systempla/lab_m_1/di/AppComponent.java
@@ -4,10 +4,11 @@ import javax.inject.Singleton;
 
 import dagger.Component;
 import ru.systempla.lab_m_1.MainActivity;
+import ru.systempla.lab_m_1.di.modules.AppModule;
 
 @Singleton
 @Component(modules = {
-
+        AppModule.class,
 })
 public interface AppComponent {
     void inject(MainActivity mainActivity);
